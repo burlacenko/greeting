2e54db46f7f3529495f426ee9fdc292ffbd1d452
Burlacenko <87153573+burlacenko@users.noreply.github.com>
2021-08-31 10:49:20 -0300

    Adjusted relative path for jest

--- hello.test.js
+++ hello.test.js
@@ -1,5 +1,10 @@
 var utils  = require('course-utilities');
+
+// if npm test is been called from "mitfacts":
 var hello = utils.load('./hello.js', 'hello');
+
+// if npm test is been called from "mit forks", path is relative to folder from which jest (npm test) is been called:
+// var hello = utils.load('./greeting/hello.js', 'hello');
 
 test('outputs the correct string', () => {
   expect(hello()).toBe("Hello World!");
